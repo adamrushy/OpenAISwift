ea028b852e3d769b94d79e98d1595e5c8d1f089f
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-12-06 19:54:46 +0000

    Add `OpenAIModelType` to make model selection type safe. model parameter is set to `OpenAIModelType.gpt3(.davinci)` by default in the `sendCompletion` method

---
+++ Sources/OpenAISwift/Models/OpenAIModelType.swift
@@ -0,0 +1,67 @@
+//
+//  OpenAIModelType.swift
+//  
+//
+//  Created by Yash Shah on 06/12/2022.
+//
+
+import Foundation
+
+/// The type of model used to generate the output
+public enum OpenAIModelType {
+	/// ``GPT3`` Family of Models
+	case gpt3(GPT3)
+
+	/// ``Codex`` Family of Models
+	case codex(Codex)
+
+	public var modelName: String {
+		switch self {
+			case .gpt3(let model): return model.rawValue
+			case .codex(let model): return model.rawValue
+		}
+	}
+
+	/// A set of models that can understand and generate natural language
+	///
+	/// [GPT-3 Models OpenAI API Docs](https://beta.openai.com/docs/models/gpt-3)
+	public enum GPT3: String {
+
+		/// Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.
+		///
+		/// > Model Name: text-davinci-003
+		case davinci = "text-davinci-003"
+
+		/// Very capable, but faster and lower cost than GPT3 ``davinci``.
+		///
+		/// > Model Name: text-curie-001
+		case curie = "text-curie-001"
+
+		/// Capable of straightforward tasks, very fast, and lower cost.
+		///
+		/// > Model Name: text-babbage-001
+		case babbage = "text-babbage-001"
+
+		/// Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.
+		///
+		/// > Model Name: text-ada-001
+		case ada = "text-ada-001"
+	}
+
+	/// A set of models that can understand and generate code, including translating natural language to code
+	///
+	/// [Codex Models OpenAI API Docs](https://beta.openai.com/docs/models/codex)
+	///
+	///  >  Limited Beta
+	public enum Codex: String {
+		/// Most capable Codex model. Particularly good at translating natural language to code. In addition to completing code, also supports inserting completions within code.
+		///
+		/// > Model Name: code-davinci-002
+		case davinci = "code-davinci-002"
+
+		/// Almost as capable as ``davinci`` Codex, but slightly faster. This speed advantage may make it preferable for real-time applications.
+		///
+		/// > Model Name: code-cushman-001
+		case cushman = "code-cushman-001"
+	}
+}
