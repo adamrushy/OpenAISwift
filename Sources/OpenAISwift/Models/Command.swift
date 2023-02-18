2c838dea104de0e063e069d73ebaff75a0f2d869
Ruxo <113531412+turangarusso@users.noreply.github.com>
2023-02-18 22:30:58 +0100

    Added temperature control
    Now you can also control the temperature parameter via the library

--- Sources/OpenAISwift/Models/Command.swift
+++ Sources/OpenAISwift/Models/Command.swift
@@ -8,10 +8,12 @@ struct Command: Encodable {
     let prompt: String
     let model: String
     let maxTokens: Int
+    let temperature: Double
     
     enum CodingKeys: String, CodingKey {
         case prompt
         case model
         case maxTokens = "max_tokens"
+        case temperature
     }
 }
