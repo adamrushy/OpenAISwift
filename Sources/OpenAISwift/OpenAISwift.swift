2eb5cb36adc7cd3193fe4f48a315528c4c6d2d1d
albertopeam <alberto__pa@hotmail.com>
2023-02-23 22:14:59 +0100

    prepare code to avoid code duplication

--- Sources/OpenAISwift/OpenAISwift.swift
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -9,8 +9,14 @@ public enum OpenAIError: Error {
     case decodingError(error: Error)
 }
 
+public struct RequestError: LocalizedError {
+    var errorDescription: String {
+        "Error creating the url"
+    }
+}
+
 public class OpenAISwift {
-    fileprivate(set) var token: String?
+    fileprivate(set) var token: String
     
     public init(authToken: String) {
         self.token = authToken
@@ -27,21 +33,7 @@ extension OpenAISwift {
     public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, temperature: Double = 1, completionHandler: @escaping (Result<OpenAI<TextResult>, OpenAIError>) -> Void) {
         let endpoint = Endpoint.completions
         let body = Command(prompt: prompt, model: model.modelName, maxTokens: maxTokens, temperature: temperature)
-        let request = prepareRequest(endpoint, body: body)
-        
-        makeRequest(request: request) { result in
-            switch result {
-            case .success(let success):
-                do {
-                    let res = try JSONDecoder().decode(OpenAI<TextResult>.self, from: success)
-                    completionHandler(.success(res))
-                } catch {
-                    completionHandler(.failure(.decodingError(error: error)))
-                }
-            case .failure(let failure):
-                completionHandler(.failure(.genericError(error: failure)))
-            }
-        }
+        makeRequest(endpoint: endpoint, body: body, completionHandler: completionHandler)
     }
     
     /// Send a Edit request to the OpenAI API
@@ -53,21 +45,27 @@ extension OpenAISwift {
     public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "", completionHandler: @escaping (Result<OpenAI<TextResult>, OpenAIError>) -> Void) {
         let endpoint = Endpoint.edits
         let body = Instruction(instruction: instruction, model: model.modelName, input: input)
-        let request = prepareRequest(endpoint, body: body)
-        
-        makeRequest(request: request) { result in
-            switch result {
-            case .success(let success):
+        makeRequest(endpoint: endpoint, body: body, completionHandler: completionHandler)
+    }
+    
+    private func makeRequest<BodyType: Encodable>(endpoint: Endpoint, body: BodyType, completionHandler: @escaping (Result<OpenAI, OpenAIError>) -> Void) {
+        guard let request = prepareRequest(endpoint, body: body) else {
+            completionHandler(.failure(.decodingError(error: RequestError())))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(.genericError(error: error)))
+            } else if let data = data {
                 do {
-                    let res = try JSONDecoder().decode(OpenAI<TextResult>.self, from: success)
+                    let res = try JSONDecoder().decode(OpenAI.self, from: data)
                     completionHandler(.success(res))
                 } catch {
                     completionHandler(.failure(.decodingError(error: error)))
                 }
-            case .failure(let failure):
-                completionHandler(.failure(.genericError(error: failure)))
             }
         }
+        task.resume()
     }
     
     /// Send a Chat request to the OpenAI API
@@ -110,16 +108,20 @@ extension OpenAISwift {
         task.resume()
     }
     
-    private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
-        var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAgainstBaseURL: true)
-        urlComponents?.path = endpoint.path
-        var request = URLRequest(url: urlComponents!.url!)
-        request.httpMethod = endpoint.method
-        
-        if let token = self.token {
-            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+    private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest? {
+        guard let baseUrl = URL(string: endpoint.baseURL()) else {
+            return nil
         }
-        
+        guard var urlComponents = URLComponents(url: baseUrl, resolvingAgainstBaseURL: true) else {
+            return nil
+        }
+        urlComponents.path = endpoint.path
+        guard let url = urlComponents.url else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.setValue("application/json", forHTTPHeaderField: "content-type")
         
         let encoder = JSONEncoder()
