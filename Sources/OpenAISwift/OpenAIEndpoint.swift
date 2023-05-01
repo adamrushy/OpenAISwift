63de2ae57791b7a93781f27dc04bcfe4de72a5a9
Alexey Strokin <alex.strok@gmail.com>
2023-05-01 18:23:24 +0300

    add custom proxy support

--- Sources/OpenAISwift/OpenAIEndpoint.swift
+++ Sources/OpenAISwift/OpenAIEndpoint.swift
@@ -4,41 +4,51 @@
 
 import Foundation
 
-enum Endpoint {
-    case completions
-    case edits
-    case chat
-    case images
-    case embeddings
-}
-
-extension Endpoint {
-    var path: String {
-        switch self {
-            case .completions:
-                return "/v1/completions"
-            case .edits:
-                return "/v1/edits"
-            case .chat:
-                return "/v1/chat/completions"
-            case .images:
-                return "/v1/images/generations"
-            case .embeddings:
-            return "/v1/embeddings"
+public struct OpenAIEndpointProvider {
+    public enum API {
+        case completions
+        case edits
+        case chat
+        case images
+        case embeddings
+    }
+    
+    public enum Source {
+        case openAI
+        case proxy(path: ((API) -> String), method: ((API) -> String))
+    }
+    
+    public let source: Source
+    
+    func getPath(api: API) -> String {
+        switch source {
+        case .openAI:
+            switch api {
+                case .completions:
+                    return "/v1/completions"
+                case .edits:
+                    return "/v1/edits"
+                case .chat:
+                    return "/v1/chat/completions"
+                case .images:
+                    return "/v1/images/generations"
+                case .embeddings:
+                    return "/v1/embeddings"
+            }
+        case let .proxy(path: pathClosure, method: _):
+            return pathClosure(api)
         }
     }
     
-    var method: String {
-        switch self {
+    func getMethod(api: API) -> String {
+        switch source {
+        case .openAI:
+            switch api {
             case .completions, .edits, .chat, .images, .embeddings:
-            return "POST"
-        }
-    }
-    
-    func baseURL() -> String {
-        switch self {
-            case .completions, .edits, .chat, .images, .embeddings:
-            return "https://api.openai.com"
+                return "POST"
+            }
+        case let .proxy(path: _, method: methodClosure):
+            return methodClosure(api)
         }
     }
 }
