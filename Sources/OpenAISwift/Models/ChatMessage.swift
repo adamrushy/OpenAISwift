89eefec0c33402ae4c43f961b12116299e3de8da
Matt Manion <mmanion10@gmail.com>
2023-07-30 16:53:15 -0400

    Update ChatMessage.swift, remove UUID

--- Sources/OpenAISwift/Models/ChatMessage.swift
+++ Sources/OpenAISwift/Models/ChatMessage.swift
@@ -18,9 +18,7 @@ public enum ChatRole: String, Codable {
 }
 
 /// A structure that represents a single message in a chat conversation.
-public struct ChatMessage: Codable, Identifiable {
-    // uuid to conform to Identifiable protocol
-    public var id = UUID()
+public struct ChatMessage: Codable {
     /// The role of the sender of the message.
     public let role: ChatRole?
     /// The content of the message.
